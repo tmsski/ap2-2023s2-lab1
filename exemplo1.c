757020af543a2bac0286e3d959f2de075c3a1303
tmsski <20782956+tmsski@users.noreply.github.com>
2023-08-06 23:45:52 -0300

    Create exemplo1.c

---
+++ exemplo1.c
@@ -0,0 +1,15 @@
+/* Lab. 1 
+ * Exemplo 1 */
+ 
+#include <stdio.h>
+
+int main() {
+    // imprime algumas mensagens na tela
+    printf("Mackenzie - FCI\n");
+    printf("Algoritmos e ");
+    printf("Prog. II\n");
+    
+    // pausa antes de terminar o programa
+    system("pause");
+    return 0;
+}
